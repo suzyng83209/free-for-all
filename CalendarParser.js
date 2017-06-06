f2c978022877ed4359f373762919472c45ebecc1
Susan Li <suzyn.one@gmail.com>
2017-06-06 02:37:20 -0400

    done

--- CalendarParser.js
+++ CalendarParser.js
@@ -1,36 +1,113 @@
-const moment = require("moment");
+const Moment = require("moment");
+const MomentRange = require("moment-range");
+const moment = MomentRange.extendMoment(Moment);
 const CsvReader = require("promised-csv");
 const reader = new CsvReader();
 
 const events = [];
-const freeWeek = [];
 
 const findFreeTime = events => {
-  const freeDay = {
-    date: moment().add
-  };
+  const freeTimeThisWeek = [];
+
+  for (let i = 0; i < 8; i += 1) {
+    const sameDayEvents = [];
+    const date = moment().add(i, "days").format("YYYY-MM-DD");
+    events.map(event => {
+      if (event.startDate.isSame(date, "day")) {
+        sameDayEvents.push(event);
+      }
+    });
+
+    sameDayEvents.sort((a, b) => a.startDate > b.startDate);
+
+    let index = 0;
+
+    for (let j = 0; j < sameDayEvents.length; j += 1) {
+      const isOverlap = (ind, arr, count) => {
+        return arr[ind - 1].startDate.isSameOrBefore(arr[count].endDate);
+      };
+      if (index !== 0 && isOverlap(index, sameDayEvents, j)) {
+        while (index !== 0 && isOverlap(index, sameDayEvents, j)) {
+          sameDayEvents[index - 1].endDate = moment.max(
+            sameDayEvents[index - 1].endDate,
+            sameDayEvents[j].endDate
+          );
+          sameDayEvents[index - 1].startDate = moment.max(
+            sameDayEvents[index - 1].startDate,
+            sameDayEvents[j].startDate
+          );
+          index -= 1;
+        }
+      } else {
+        sameDayEvents[index] = sameDayEvents[j];
+      }
+
+      index += 1;
+    }
+
+    const freeRanges = [];
+    let freeTimeStart = moment(date).add(8, "hours");
+    const freeTimeEnd = moment(date).add(22, "hours");
+    for (let k = 0; k < index; k += 1) {
+      if (freeTimeStart.isBefore(sameDayEvents[k].startDate)) {
+        freeRanges.push(
+          moment.range(freeTimeStart, sameDayEvents[k].startDate)
+        );
+      }
+      freeTimeStart = sameDayEvents[k].endDate;
+    }
+    freeRanges.push(moment.range(freeTimeStart, freeTimeEnd));
+
+    let maxRange = freeRanges[0];
+    for (let l = 0; l < freeRanges.length; l += 1) {
+      if (maxRange < freeRanges[l]) {
+        maxRange = freeRanges[l];
+      }
+    }
+
+    freeTimeThisWeek.push(maxRange);
+  }
+
+  let maxRangeThisWeek = freeTimeThisWeek[0];
+  for (let m = 0; m < freeTimeThisWeek.length; m += 1) {
+    if (maxRangeThisWeek < freeTimeThisWeek[m]) {
+      maxRangeThisWeek = freeTimeThisWeek[m];
+    }
+  }
+
+  return maxRangeThisWeek;
+};
+
+const isWithinWeek = date => moment(date).isBefore(moment().add(8, "days"));
+
+const isWithinDayTime = date => {
+  const timeFormat = "hh:mm:ss";
+  const startTime = moment("08:00:00", timeFormat);
+  const endTime = moment("22:00:00", timeFormat);
+
+  return moment(date).isBetween(startTime, endTime);
 };
 
 reader.on("row", data => {
   // checks up to 1 week from request time
-  const timeFormat = "hh:mm:ss";
-  const startTime = moment("08:00:00", timeFormat);
-  const endTime = moment("22:00:00", timeFormat);
-  const isWithinWeek = moment(data[1]).isBefore(moment().add(8, "days"));
-  const isWithinDayTime = moment(data[1]).isBetween(startTime, endTime);
-
-  if (isWithinDayTime && isWithinWeek) {
-    // convert raw data into json format with date objects
-    const jsonData = {
-      userId: data[0],
+  if (isWithinWeek(data[1])) {
+    const busyRange = {
       startDate: moment(data[1]),
       endDate: moment(data[2])
     };
-
-    events.push(jsonData);
+    events.push(busyRange);
   }
   return;
 });
 
 // read raw data into events array and operate on it
-reader.read("calendar.csv", events).then(findFreeTime);
+reader.read("calendar.csv", events).then(events => {
+  const maxFreeRangeThisWeek = findFreeTime(events).toDate();
+  const startTime = moment(maxFreeRangeThisWeek[0]).format(
+    "YYYY-MM-DD hh:mm:ss"
+  );
+  const endTime = moment(maxFreeRangeThisWeek[1]).format("YYYY-MM-DD hh:mm:ss");
+  console.log(
+    "Everyone should meet this week from " + startTime + " to " + endTime
+  );
+});
