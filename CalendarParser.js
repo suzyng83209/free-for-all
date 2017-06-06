7ea3de40f06022b4cca1a24d4cb997655946be56
Susan Li <suzyn.one@gmail.com>
2017-06-06 02:37:06 -0400

    done

--- CalendarParser.js
+++ CalendarParser.js
@@ -3,17 +3,23 @@ const CsvReader = require("promised-csv");
 const reader = new CsvReader();
 
 const events = [];
-const freeBlocks = [];
+const freeWeek = [];
 
 const findFreeTime = events => {
-  console.log(events);
+  const freeDay = {
+    date: moment().add
+  };
 };
 
 reader.on("row", data => {
-
   // checks up to 1 week from request time
-  if (moment(data[1]).isBefore(moment().add(8, "days"))) {
+  const timeFormat = "hh:mm:ss";
+  const startTime = moment("08:00:00", timeFormat);
+  const endTime = moment("22:00:00", timeFormat);
+  const isWithinWeek = moment(data[1]).isBefore(moment().add(8, "days"));
+  const isWithinDayTime = moment(data[1]).isBetween(startTime, endTime);
 
+  if (isWithinDayTime && isWithinWeek) {
     // convert raw data into json format with date objects
     const jsonData = {
       userId: data[0],
